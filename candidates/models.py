ba35b283ccb590a04d3fd9f08968367f041e0029
raulbreton <raul.elizalde.breton@gmail.com>
2023-11-06 19:36:34 -0600

    Candidate model creation

--- candidates/models.py
+++ candidates/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from users.models import CustomUser
+from django.db.models.signals import post_save
 
-# Create your models here.
+class CandidateProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, default=None)
+    resumen = models.TextField()
+    experiencia = models.TextField()
+    educacion = models.TextField()
+    habilidades = models.TextField()  # Almacena como texto JSON
+    idiomas = models.TextField()      # Almacena como texto JSON
+    certificaciones = models.TextField()  # Almacena como texto JSON
+
+    def __str__(self):
+        return self.user.email
+    
+def created_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = CandidateProfile(user=instance)
+        user_profile.save()
+
+post_save.connect(created_profile, sender=CustomUser)
--- openhire/settings.py
+++ openhire/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'users',
     'employers',
     'job_offers',
+    'candidates',
 ]
 
 MIDDLEWARE = [
